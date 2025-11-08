f908b408f8ed55f6198843448e13a8da19f84fa5
ogcelio <joaocelioaraujosander@gmail.com>
2025-11-08 19:43:45 -0300

    changing the benchmark format

--- benchmark_methods.py
+++ benchmark_methods.py
@@ -3,27 +3,20 @@ from metodos.med_main import med
 from metodos.med_mod_main import med_mod
 from metodos.dd_main import diamond_difference
 from metodos.rm_main import response_matrix
+from metodos.sdm_main import sdm
+from metodos.msd_main import msd
 import numpy as np
 from time import perf_counter
 
 
-""" N = 4
-cce = 1
-ccd = 0
-prec = 1e-8
-NN = [1, 1]
-regs = [1, 2]
-n_regs = 2
-esp_R = [5, 5] """
-
 N = 4
 cce = 1
 ccd = 0
-prec = 10e-6
-NN = [1, 1]
-regs = [1, 2]
-n_regs = 2
-esp_R = [5, 5]
+prec = 1e-8
+NN = [1, 1, 1, 1]
+regs = [1, 2, 3, 4]
+n_regs = 4
+esp_R = [5, 5, 5, 5]
 
 with open("benchmark_data.json", "r", encoding="utf8") as file:
     propriedades_1grupo = json.load(file)
@@ -43,41 +36,54 @@ for i in range(len(sigmaT)):
     Qj[i] = float(Qj[i])
     NiSigmaF[i] = float(NiSigmaF[i])
 
-final_fi, psiX, iteracao, abs_rate, escape_rate, execution_time = med(
-    sigmaT,
-    sigmaS0,
-    sigmaS1,
-    sigmaS2,
-    Qj,
-    NiSigmaF,
-    N,
-    cce,
-    ccd,
-    prec,
-    NN,
-    regs,
-    n_regs,
-    esp_R,
-)
-print(psiX)
+sum_initial = []
+for i in range(1000):
+    final_fi, psi, iteration, abs_rate, escape_rate, execution_time = med_mod(
+        sigmaT,
+        sigmaS0,
+        sigmaS1,
+        sigmaS2,
+        Qj,
+        NiSigmaF,
+        N,
+        cce,
+        ccd,
+        prec,
+        NN,
+        regs,
+        n_regs,
+        esp_R,
+    )
+    sum_initial.append(execution_time)
+
+initial = sum(sum_initial) / 1000
+print(initial)
 
 print(50 * "-")
 
-final_fi, psiX, iteracao, escape_rate, execution_time = response_matrix(
-    sigmaT,
-    sigmaS0,
-    sigmaS1,
-    sigmaS2,
-    Qj,
-    NiSigmaF,
-    N,
-    cce,
-    ccd,
-    prec,
-    NN,
-    regs,
-    n_regs,
-    esp_R,
-)
+sum_final = []
+for i in range(1000):
+    final_fi, psi, iteration, abs_rate, escape_rate, execution_time = msd(
+        sigmaT,
+        sigmaS0,
+        sigmaS1,
+        sigmaS2,
+        Qj,
+        NiSigmaF,
+        N,
+        cce,
+        ccd,
+        prec,
+        NN,
+        regs,
+        n_regs,
+        esp_R,
+    )
+    sum_final.append(execution_time)
 
-print(psiX)
+final = sum(sum_final) / 1000
+print(final)
+
+print(50 * "-")
+
+print((abs(final - initial) / initial) * 100)
